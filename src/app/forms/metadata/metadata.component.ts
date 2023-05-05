ff4218b52304c603d05418b71d358ddd6e06d5a4
Philippe Martin <phmartin@redhat.com>
2023-05-05 20:10:01 +0200

    Document metadata fields

--- src/app/forms/metadata/metadata.component.ts
+++ src/app/forms/metadata/metadata.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { StateService } from 'src/app/services/state.service';
 import { WasmGoService } from 'src/app/services/wasm-go.service';
 
+const semverPattern = `^([0-9]+)\\.([0-9]+)\\.([0-9]+)(\\-[0-9a-z-]+(\\.[0-9a-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$`;
 
 @Component({
   selector: 'app-metadata',
@@ -10,25 +11,29 @@ import { WasmGoService } from 'src/app/services/wasm-go.service';
   styleUrls: ['./metadata.component.css']
 })
 export class MetadataComponent implements OnInit {
-  
-  name = new FormControl('');
-  version = new FormControl('');
-  displayName = new FormControl('');
-  description = new FormControl('');
-  tags = new FormControl("");
-  architectures = new FormControl("");
-  icon = new FormControl("");
-  globalMemoryLimit = new FormControl("");
-  projectType = new FormControl("");
-  language = new FormControl("");
-  website = new FormControl("");
-  provider = new FormControl("");
-  supportUrl = new FormControl("");
+
+  form: FormGroup;
 
   constructor(
     private wasm: WasmGoService,
     private state: StateService,
-  ) {}
+  ) {
+    this.form = new FormGroup({
+      name: new FormControl(''),
+      version: new FormControl('', Validators.pattern(semverPattern)),
+      displayName: new FormControl(''),
+      description: new FormControl(''),
+      tags: new FormControl(""),
+      architectures: new FormControl(""),
+      icon: new FormControl(""),
+      globalMemoryLimit: new FormControl(""),
+      projectType: new FormControl(""),
+      language: new FormControl(""),
+      website: new FormControl(""),
+      provider: new FormControl(""),
+      supportUrl: new FormControl(""),        
+    });
+  }
 
   ngOnInit() {
     this.state.state.subscribe(async newContent => {
@@ -36,38 +41,12 @@ export class MetadataComponent implements OnInit {
       if (metadata == null) {
         return
       }
-      this.name.setValue(metadata.name);
-      this.version.setValue(metadata.version);
-      this.displayName.setValue(metadata.displayName);
-      this.description.setValue(metadata.description);
-      this.tags.setValue(metadata.tags)
-      this.architectures.setValue(metadata.architectures)
-      this.icon.setValue(metadata.icon)
-      this.globalMemoryLimit.setValue(metadata.globalMemoryLimit)
-      this.projectType.setValue(metadata.projectType)
-      this.language.setValue(metadata.language)
-      this.website.setValue(metadata.website)
-      this.provider.setValue(metadata.provider)
-      this.supportUrl.setValue(metadata.supportUrl)
+      this.form.setValue(metadata);
     });
   }
 
   onSave() {
-    const newDevfile = this.wasm.setMetadata({
-      name: this.name.value,
-      version: this.version.value,
-      displayName: this.displayName.value,
-      description: this.description.value,
-      tags: this.tags.value,
-      architectures: this.architectures.value,
-      icon: this.icon.value,
-      globalMemoryLimit: this.globalMemoryLimit.value,
-      projectType: this.projectType.value,
-      language: this.language.value,
-      website: this.website.value,
-      provider: this.provider.value,
-      supportUrl: this.supportUrl.value,
-    })
+    const newDevfile = this.wasm.setMetadata(this.form.value);
     this.state.changeDevfileYaml(newDevfile);
   }
 }
